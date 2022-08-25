c0245372f10feef2c2fa5f2da05da8790af55d9c
oumanatsumi <oumanatsumi@126.com>
2022-08-25 10:58:11 +0800

    commit20220822

---
+++ Q191/Solution.java
@@ -0,0 +1,16 @@
+package Q191;
+
+public class Solution {
+    // you need to treat n as an unsigned value
+    public int hammingWeight(int n) {
+        int res = 0;
+        for(int i =0;i<32;i++)
+        {
+            if((n & (1 << i))!= 0)
+            {
+                res++;
+            }
+        }
+        return res;
+    }
+}
