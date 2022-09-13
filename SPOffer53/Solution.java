13f4590ad8be8692d603c63482837d6d06b3a5ed
HuangYijun <oumanatsumi@126.com>
2022-09-13 12:08:15 +0800

    commit20220913

--- SPOffer53/Solution.java
+++ SPOffer53/Solution.java
@@ -2,27 +2,17 @@ package SPOffer53;
 
 public class Solution {
     // 暴力
-//    public int search(int[] nums, int target) {
-//        int ans = 0;
-//        for(int i:nums){
-//            if(target<i){
-//                return ans;
-//            }else if(target == i){
-//                ans++;
-//            }
-//        }
-//        return ans;
-//    }
-
-    // 二分
     public int search(int[] nums, int target) {
-        int left =0,right = nums.length-1;
         int ans = 0;
-        while(left<right){
-            int mid = (left + right)/2;
-            if(target == mid){
-                ans ++;
+        for(int i:nums){
+            if(target<i){
+                return ans;
+            }else if(target == i){
+                ans++;
             }
         }
+        return ans;
     }
+
+
 }
