2ee2d6bffe196164cabef6de3c7d0d336521747a
oumanatsumi <oumanatsumi@126.com>
2022-09-14 22:25:04 +0800

    commit2022090142

--- SPOffer26/Solution.java
+++ SPOffer26/Solution.java
@@ -12,28 +12,47 @@ class TreeNode {
 
 public class Solution {
     public boolean isSubStructure(TreeNode A, TreeNode B) {
-        StringBuilder sba = new StringBuilder("");
-        StringBuilder sbb = new StringBuilder("");
-        midOrder(sba,A);
-        midOrder(sbb,B);
+        String sba = midOrder("",A);
+        String sbb = midOrder("",B);
+
+        if(sba.length()==0 || sbb.length()==0){
+            return false;
+        }
         System.out.println(sba);
         System.out.println(sbb);
-        for(int i = 0;i<sba.length()-sbb.length();i++){
-            if(sba.substring(i,i+sbb.length()).equals(sbb)){
+        for(int i = 0;i<sba.length()-sbb.length()+1;i++){
+            if(sba.substring(i,i+sbb.length()).equals(sbb.toString())){
                 return true;
             }
         }
         return false;
     }
 
-    private void midOrder(StringBuilder s,TreeNode root){
+    private String midOrder(String s,TreeNode root){
         if(root == null) {
-            s.append(" ");
-            return;
+            s += " ";
+            return s;
         }
-        midOrder(s, root.left);
-        s.append(root.val);
-        s.append("_");
-        midOrder(s, root.right);
+        s = midOrder(s, root.left);
+        s += root.val;
+        s += "_";
+        s = midOrder(s, root.right);
+        return s;
+    }
+
+    public static void main(String[] args) {
+        Solution s = new Solution();
+        TreeNode t1 = new TreeNode(1);
+        TreeNode t2 = new TreeNode(2);
+        TreeNode t3 = new TreeNode(3);
+        TreeNode t4 = new TreeNode(4);
+        TreeNode t5 = new TreeNode(3);
+
+        t1.left =t2;
+        t1.right=t3;
+        t2.left =t4;
+
+        boolean res = s.isSubStructure(t1,t5);
+        System.out.println(res);
     }
 }
