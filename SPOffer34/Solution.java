92fe2dc409737d005aec15c483c99cb3474c98e7
HuangYijun <oumanatsumi@126.com>
2022-10-14 17:54:04 +0800

    commit20221014

---
+++ SPOffer34/Solution.java
@@ -0,0 +1,28 @@
+package SPOffer34;
+
+import java.util.List;
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode() {
+    }
+
+    TreeNode(int val) {
+        this.val = val;
+    }
+
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+
+public class Solution {
+    public List<List<Integer>> pathSum(TreeNode root, int target) {
+
+    }
+}
