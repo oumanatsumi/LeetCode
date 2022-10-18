40e3a9d90116d661cc69a0bb6a9b14d8916f1e46
HuangYijun <oumanatsumi@126.com>
2022-10-18 18:07:26 +0800

    commit20221018

--- SPOffer35/Solution.java
+++ SPOffer35/Solution.java
@@ -15,15 +15,38 @@ class Node {
     }
 }
 public class Solution {
-    Map<Node,Node> nodeMap = new HashMap<>();
+//    Map<Node,Node> nodeMap = new HashMap<>();
+//    public Node copyRandomList(Node head) {
+//        if(head == null) return null;
+//        if(!nodeMap.containsKey(head)){
+//            Node newNode = new Node(head.val);
+//            nodeMap.put(head,newNode);
+//            newNode.next = copyRandomList(head.next);
+//            newNode.random = nodeMap.getOrDefault(head.random, null);
+//        }
+//        return nodeMap.get(head);
+//    }
+
+
     public Node copyRandomList(Node head) {
-        if(head == null) return null;
-        if(!nodeMap.containsKey(head)){
+        Map<Node,Node> map = new HashMap<>();
+        Node headCopy = head;
+        Node h = new Node(-1);
+        Node cur = h;
+        while (head!= null){
             Node newNode = new Node(head.val);
-            nodeMap.put(head,newNode);
-            newNode.next = copyRandomList(head.next);
-            newNode.random = nodeMap.getOrDefault(head.random, null);
+            map.put(head,newNode);
+            cur.next = newNode;
+            cur = cur.next;
+            head = head.next;
         }
-        return nodeMap.get(head);
+        cur = h.next;
+        while (headCopy!= null){
+            cur.random = map.get(headCopy.random);
+            cur = cur.next;
+            headCopy = headCopy.next;
+        }
+        return h.next;
     }
+
 }
---
+++ SPOffer36/Solution.java
@@ -0,0 +1,51 @@
+package SPOffer36;
+
+import java.util.ArrayList;
+import java.util.List;
+
+class Node {
+    public int val;
+    public Node left;
+    public Node right;
+
+    public Node() {}
+
+    public Node(int _val) {
+        val = _val;
+    }
+
+    public Node(int _val,Node _left,Node _right) {
+        val = _val;
+        left = _left;
+        right = _right;
+    }
+};
+
+public class Solution {
+    List<Node> nodes = new ArrayList<>();
+    public Node treeToDoublyList(Node root) {
+        if(root == null) return null;
+        mid(root);
+        if(nodes.size() == 1){
+            root.left = root;
+            root.right = root;
+            return root;
+        }
+        for (int i = 1; i < nodes.size()-1; i++) {
+            nodes.get(i).left = nodes.get(i-1);
+            nodes.get(i).right = nodes.get(i+1);
+        }
+        nodes.get(0).left = nodes.get(nodes.size()-1);
+        nodes.get(0).right = nodes.get(1);
+        nodes.get(nodes.size()-1).right = nodes.get(0);
+        nodes.get(nodes.size()-1).left = nodes.get(nodes.size()-2);
+        return nodes.get(0);
+    }
+
+    private void mid(Node root){
+        if(root == null) return;
+        mid(root.left);
+        nodes.add(root);
+        mid(root.right);
+    }
+}
