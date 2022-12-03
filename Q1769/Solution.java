d5593cae6ae64935fe1cbb1a635cd3c39997db1c
oumanatsumi <oumanatsumi@126.com>
2022-12-03 22:19:05 +0800

    commit2022123

---
+++ Q1769/Solution.java
@@ -0,0 +1,21 @@
+package Q1769;
+
+public class Solution {
+    public int[] minOperations(String boxes) {
+        int n = boxes.length();
+        int[] cnt = new int[n];
+        cnt[0] = boxes.charAt(0) == '0'? 0 :1 ;
+        for (int i = 1; i < n; i++) {
+            if(boxes.charAt(i) == '1') cnt[i] = cnt[i-1]+1;
+            else cnt[i] = cnt[i-1];
+        }
+        int[] ans = new int[n];
+        for (int i = 1; i < n; i++) {
+            if(boxes.charAt(i) == '1') ans[0]+=i;
+        }
+        for (int i = 1; i < n; i++) {
+            ans[i] = ans[i-1] + cnt[i-1] - (cnt[n-1] - cnt[i-1]);
+        }
+        return ans;
+    }
+}
