851427ffd3963775d428ce101cdc17244550e537
oumanatsumi <oumanatsumi@126.com>
2023-02-04 22:31:43 +0800

    commit20230129

---
+++ Q2319/Solution.java
@@ -0,0 +1,17 @@
+package Q2319;
+
+public class Solution {
+    public boolean checkXMatrix(int[][] grid) {
+        int size = grid.length;
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+                if(i == j || i + j == size -1){
+                    if(grid[i][j] == 0) return false;
+                }else {
+                    if(grid[i][j] != 0) return false;
+                }
+            }
+        }
+        return true;
+    }
+}
