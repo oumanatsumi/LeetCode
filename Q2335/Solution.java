e953c25caf2fde8223cfe47ebd2e44f58702a0d7
oumanatsumi <oumanatsumi@126.com>
2023-02-11 23:25:57 +0800

    commit20230211

---
+++ Q2335/Solution.java
@@ -0,0 +1,18 @@
+package Q2335;
+
+import java.util.Arrays;
+
+public class Solution {
+    public int fillCups(int[] amount) {
+        int cnt = 0;
+        Arrays.sort(amount);
+        while (amount[1] != 0){
+            amount[2] --;
+            amount[1] --;
+            cnt ++;
+            Arrays.sort(amount);
+        }
+        cnt += amount[2];
+        return cnt;
+    }
+}
