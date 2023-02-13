d63d5d484693da692b6854cc340ebf05c757ed18
oumanatsumi <oumanatsumi@126.com>
2023-02-13 09:26:37 +0800

    commit20230212

---
+++ QC332_1/Solution.java
@@ -0,0 +1,17 @@
+package QC332_1;
+
+public class Solution {
+    public long findTheArrayConcVal(int[] nums) {
+        int left = 0,right = nums.length-1;
+        long res = 0L;
+        while(left < right){
+            res += Integer.parseInt(String.valueOf(nums[left]) + String.valueOf(nums[right]));
+            left ++;
+            right --;
+        }
+        if(left == right){
+            res += nums[left];
+        }
+        return res;
+    }
+}
