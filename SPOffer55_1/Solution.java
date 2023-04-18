f147b2482748a26cd2ad1bb0c32797f47c6b3ab0
黄一骏（21软） <512717388@qq.com>
2023-04-18 18:26:52 +0800

    commit202304018

---
+++ SPOffer55_1/Solution.java
@@ -0,0 +1,15 @@
+package SPOffer55_1;
+
+   class TreeNode {
+      int val;
+      TreeNode left;
+      TreeNode right;
+      TreeNode(int x) { val = x; }
+  }
+
+public class Solution {
+   public int maxDepth(TreeNode root) {
+      return root == null ? 0 : Math.max(maxDepth(root.left)+1,maxDepth(root.right)+1);
+   }
+
+}
