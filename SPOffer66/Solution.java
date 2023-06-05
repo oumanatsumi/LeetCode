5c355bd1c6522b62f6df77c0499b2be056790138
黄一骏（21软） <512717388@qq.com>
2023-06-05 10:57:22 +0800

    commit20230605

--- SPOffer66/Solution.java
+++ SPOffer66/Solution.java
@@ -1,9 +1,26 @@
 package SPOffer66;
 
-//public class Solution {
-//    public int[] constructArr(int[] a) {
-//        int n = a.length;
-//        int[] prefix = new int[n];
-//
-//    }
-//}
+public class Solution {
+    public int[] constructArr(int[] a) {
+        int n = a.length;
+        int[] left = new int[n];
+        int[] right = new int[n];
+        int[] res = new int[n];
+        if(a.length == 0) return a;
+        if(a.length == 1) return new int[]{0};
+        left[0] = a[0];
+        right[n-1] = a[n-1];
+        for (int i = 1; i < n; i++) {
+            left[i] = left[i-1] * a[i];
+        }
+        for (int i = n-2; i > 0 ; i--) {
+            right[i] = right[i+1] * a[i];
+        }
+        for (int i = 0; i < n; i++) {
+            res[i] = 1;
+            if(i!=0) res[i] *= left[i-1];
+            if(i!=n-1) res[i] *= right[i+1];
+        }
+        return res;
+    }
+}
